a8df66d83e3415f223b006a8f7eb61353dd868b5
abhijithabhi01 <iamabhijithabhi01@gmail.com>
2025-07-03 00:15:26 +0530

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./navbar.css"
+function Navbar() {
+  return (
+    // NAVBAR 
+    <div id='navbar'>
+      {/* LOGO */}
+      <div>
+        <h2>application</h2>
+      </div>
+
+      {/* ITEMS */}
+      <div id='nav-items'>
+        <div>
+          <p>HOME</p>
+        </div>
+
+        <div>
+          <p>HOME</p>
+        </div>
+
+        <div>
+          <p>HOME</p>
+        </div>
+
+        <div>
+          <p>HOME</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
